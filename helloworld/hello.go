8fa1778789602afcc9d36d2be3719a24c51f46d5
Krystian Fowler <krystian@stratokit.io>
2023-09-20 20:51:05 +0200

    Hello, World

---
+++ helloworld/hello.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+const (
+	french             = "French"
+	spanish            = "Spanish"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return languagePrefix(language) + name
+}
+
+func languagePrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("World", ""))
+}
